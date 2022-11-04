4abf6aff5b6ffed1ecfc57259bfab26f748c860a
SniverDaBest <owen.gary629@gmail.com>
2022-11-04 16:54:07 -0700

    The first compiler bugfix
    I had something from a beta...

--- compile.bat
+++ compile.bat
@@ -1,13 +1,10 @@
 @echo off
 
 echo !=== COMPILING ===!
-pyinstaller lnavHelp.py --onefile -i iconHlp.ico
 pyinstaller main.py -i icon.ico --noconfirm
 cd dist\main
 rename main.exe lnav.exe
-echo e >> lnavHelp.exe
 cd ..
-xcopy lnavHelp.exe main\lnavHelp.exe
 cd ..
 echo !=== FINISHED COMPILING ===!
 
